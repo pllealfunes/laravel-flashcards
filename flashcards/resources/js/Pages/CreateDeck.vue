8ae4fdb4f06d566c35ef1f250b497f89227a31be
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2024-02-06 20:28:00 -0500

    changed points and difficulty lvl to selects

--- flashcards/resources/js/Pages/CreateDeck.vue
+++ flashcards/resources/js/Pages/CreateDeck.vue
@@ -14,8 +14,13 @@ const addCard = () => {
         question: "",
         answer: "",
         hint: "",
+        difficultylevel: "",
         points: "",
     });
+};
+
+const deleteCard = (index) => {
+    form.cards.splice(index, 1);
 };
 </script>
 
@@ -33,96 +38,174 @@ const addCard = () => {
                 class="max-w-md mx-auto mt-8"
             >
                 <!-- deck title -->
-                <label
-                    for="title"
-                    class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                    >Deck Title</label
+                <div
+                    class="rounded-lg border-violet-800 p-3 bg-violet-950 mb-5"
                 >
-                <input
-                    type="text"
-                    v-model="form.title"
-                    class="w-full"
-                    name="title"
-                />
-                <div v-if="form.errors.title">{{ form.errors.title }}</div>
-                <div class="inline m-2">Cards</div>
+                    <label
+                        for="title"
+                        class="block mb-2 uppercase font-bold text-xs text-slate-50"
+                        >Deck Title</label
+                    >
+                    <input
+                        type="text"
+                        v-model="form.title"
+                        class="w-full"
+                        name="title"
+                    />
+                    <div
+                        v-if="form.errors.title"
+                        class="text-xs mt-1 text-red-100"
+                    >
+                        {{ form.errors.title }}
+                    </div>
+                </div>
                 <button
                     type="button"
-                    class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm p-2 dark:focus:ring-yellow-900"
+                    class="focus:outline-none text-white bg-violet-400 hover:bg-violet-500 focus:ring-4 focus:ring-violet-300 font-medium rounded-lg text-sm p-2 dark:focus:ring-violet-900 mb-5"
                     @click="addCard"
                 >
-                    ADD
+                    + ADD CARD
                 </button>
-                <div v-for="(card, index) in form.cards" :key="index">
+                <!-- add cards -->
+                <div
+                    v-for="(card, index) in form.cards"
+                    :key="index"
+                    class="rounded-lg border-violet-800 p-3 bg-violet-950 mb-5"
+                >
+                    <button
+                        type="button"
+                        class="focus:outline-none text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm p-2 dark:focus:ring-red-900 mb-5"
+                        @click="deleteCard(index)"
+                    >
+                        Delete
+                    </button>
                     <!-- question -->
                     <label
                         for="question"
-                        class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        >Question</label
+                        class="block mb-2 uppercase font-bold text-xs text-slate-50 m-1"
+                        >Question :</label
                     >
                     <input
                         type="text"
                         v-model="card.question"
                         class="w-full"
+                        :class="{
+                            'border-red-400':
+                                form.errors[`cards.${index}.question`],
+                        }"
                         name="question"
                         :aria-label="`Card $(index+1) question`"
                     />
-                    <div v-if="card.question">
-                        {{ card.question }}
+                    <div
+                        v-if="form.errors[`cards.${index}.question`]"
+                        class="ml-8 text-ms mt-1 text-red-600"
+                    >
+                        {{ form.errors[`cards.${index}.question`] }}
                     </div>
                     <!-- answer -->
                     <label
                         for="answer"
-                        class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        >Answer</label
+                        class="block mb-2 uppercase font-bold text-xs text-slate-50 m-1"
+                        >Answer :</label
                     >
                     <input
                         type="text"
                         v-model="card.answer"
                         class="w-full"
+                        :class="{
+                            'border-red-400':
+                                form.errors[`cards.${index}.answer`],
+                        }"
                         name="answer"
                         :aria-label="`Card $(index+1) answer`"
                     />
-                    <div v-if="card.answer">
-                        {{ card.answer }}
+                    <div
+                        v-if="form.errors[`cards.${index}.answer`]"
+                        class="ml-8 text-ms mt-1 text-red-600"
+                    >
+                        {{ form.errors[`cards.${index}.answer`] }}
                     </div>
                     <!-- hint -->
                     <label
                         for="hint"
-                        class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        >Hint</label
+                        class="block mb-2 uppercase font-bold text-xs text-slate-50 m-1"
+                        >Hint :</label
                     >
                     <input
                         type="text"
                         v-model="card.hint"
                         class="w-full"
+                        :class="{
+                            'border-red-400':
+                                form.errors[`cards.${index}.hint`],
+                        }"
                         name="hint"
                         :aria-label="`Card $(index+1) hint`"
                     />
-                    <div v-if="card.hint">
-                        {{ card.hint }}
+                    <div
+                        v-if="form.errors[`cards.${index}.hint`]"
+                        class="ml-8 text-ms mt-1 text-red-600"
+                    >
+                        {{ form.errors[`cards.${index}.hint`] }}
                     </div>
-                    <!-- points-->
+                    <!-- difficulty level -->
                     <label
                         for="points"
-                        class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        >Points</label
+                        class="block mb-2 uppercase font-bold text-xs text-slate-50 m-1"
+                        >difficulty level :</label
                     >
-                    <input
-                        type="text"
+                    <select
+                        v-model="card.difficultylevel"
+                        name="difficultylevel"
+                        :aria-label="`Card $(index+1) difficultylevel`"
+                        :class="{
+                            'border-red-400':
+                                form.errors[`cards.${index}.difficultylevel`],
+                        }"
+                    >
+                        <option disabled value="">Please select one</option>
+                        <option>easy</option>
+                        <option>intermediate</option>
+                        <option>hard</option>
+                    </select>
+                    <div
+                        v-if="form.errors[`cards.${index}.difficultylevel`]"
+                        class="ml-8 text-ms mt-1 text-red-600"
+                    >
+                        {{ form.errors[`cards.${index}.difficultylevel`] }}
+                    </div>
+                    <!-- points -->
+                    <label
+                        for="points"
+                        class="block mb-2 uppercase font-bold text-xs text-slate-50 m-1"
+                        >Points :</label
+                    >
+                    <select
                         v-model="card.points"
-                        name="points"
+                        name="difficultylevel"
                         :aria-label="`Card $(index+1) points`"
-                    />
-                    <div v-if="card.points">
-                        {{ card.points }}
+                        :class="{
+                            'border-red-400':
+                                form.errors[`cards.${index}.points`],
+                        }"
+                    >
+                        <option disabled value="">Please select one</option>
+                        <option>1</option>
+                        <option>3</option>
+                        <option>5</option>
+                    </select>
+                    <div
+                        v-if="form.errors[`cards.${index}.points`]"
+                        class="ml-8 text-ms mt-1 text-red-600"
+                    >
+                        {{ form.errors[`cards.${index}.points`] }}
                     </div>
                 </div>
                 <!-- submit -->
                 <button
                     type="submit"
                     :disabled="form.processing"
-                    class="block mt-5 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                    class="block mt-5 uppercase focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                 >
                     Submit
                 </button>
