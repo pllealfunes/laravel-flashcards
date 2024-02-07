ecb1df9c5e7f490730b23bd0045015194f654d11
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2024-02-06 20:30:10 -0500

    uninstalled fontawesome

--- flashcards/resources/js/Pages/CreateDeck.vue
+++ flashcards/resources/js/Pages/CreateDeck.vue
@@ -44,7 +44,7 @@ const deleteCard = (index) => {
                     <label
                         for="title"
                         class="block mb-2 uppercase font-bold text-xs text-slate-50"
-                        >Deck Title</label
+                        >Deck Title :</label
                     >
                     <input
                         type="text"
