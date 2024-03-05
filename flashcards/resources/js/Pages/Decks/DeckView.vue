c8206c982e292e88c653d420c3e9af9ebc3ca6e9
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2024-03-04 19:32:13 -0500

    added show/hide hint button to cards

--- flashcards/resources/js/Pages/Decks/DeckView.vue
+++ flashcards/resources/js/Pages/Decks/DeckView.vue
@@ -7,6 +7,7 @@ const { deck } = defineProps(["deck"]);
 const flippedStates = ref(new Array(deck.cards.length).fill(false));
 const activeCardIndex = ref(0); // Keep track of the index of the currently active card
 const isActive = ref(false);
+const showHint = ref(false);
 
 const toggleFlip = (index) => {
     // Update the flipped state of the clicked card
@@ -31,6 +32,12 @@ const showPrevCard = () => {
 
 // Computed property to determine if the Prev button should be disabled
 const isPrevButtonDisabled = computed(() => activeCardIndex.value === 0);
+
+const hintToggle = () => {
+    console.log(showHint.value);
+    showHint.value = !showHint.value;
+    console.log(showHint.value);
+};
 </script>
 
 <template>
@@ -40,30 +47,77 @@ const isPrevButtonDisabled = computed(() => activeCardIndex.value === 0);
                 {{ deck.title }}
             </h2>
         </template>
-        <div class="flex flex-col">
+        <div class="relative">
             <div
                 v-for="(card, index) in deck.cards"
                 :key="card.id"
                 v-show="index === activeCardIndex"
-                class="m-5 font-semibold absolute inset-x-0 flex flex-wrap justify-center items-center transition-transform duration-500 ease-in-out"
+                class="m-5 font-semibold inset-x-0 flex flex-wrap justify-center items-center transition-transform duration-500 ease-in-out"
             >
                 <div
-                    class="w-[700px] h-[450px] dark:text-slate-200 bg-white border border-gray-200 rounded-lg shadow dark:bg-sky-950 dark:border-sky-950 mb-2 w-80 h-80 flex flex-col justify-between cursor-pointer group perspective relative preserve-3d duration-1000"
+                    class="w-[700px] h-[450px] dark:text-slate-200 border border-gray-200 rounded-lg shadow dark:bg-sky-950 dark:border-sky-950 mb-2 w-80 h-80 flex flex-col justify-between cursor-pointer group perspective relative preserve-3d duration-1000"
                     :class="{
                         'flip-card': flippedStates[index],
                     }"
-                    @click="toggleFlip(index)"
+                    @click.stop="toggleFlip(index)"
                 >
                     <div class="backface-hidden w-full h-full p-4">
-                        <p>{{ card.hint }}</p>
+                        <button
+                            @click.stop="hintToggle"
+                            class="rounded-lg dark:bg-yellow-500 p-1 text-sky-950 flex flex-wrap flex-row w-20"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                class="w-6 h-6"
+                            >
+                                <path
+                                    d="M12 .75a8.25 8.25 0 0 0-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 0 0 .577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 0 1-.937-.171.75.75 0 1 1 .374-1.453 5.261 5.261 0 0 0 2.626 0 .75.75 0 1 1 .374 1.452 6.712 6.712 0 0 1-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 0 0 .577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0 0 12 .75Z"
+                                />
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M9.013 19.9a.75.75 0 0 1 .877-.597 11.319 11.319 0 0 0 4.22 0 .75.75 0 1 1 .28 1.473 12.819 12.819 0 0 1-4.78 0 .75.75 0 0 1-.597-.876ZM9.754 22.344a.75.75 0 0 1 .824-.668 13.682 13.682 0 0 0 2.844 0 .75.75 0 1 1 .156 1.492 15.156 15.156 0 0 1-3.156 0 .75.75 0 0 1-.668-.824Z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            <p>Hint</p>
+                        </button>
+                        <!-- Small Modal -->
+                        <div v-if="showHint" @click.stop="hintToggle">
+                            <div class="absolute mr-2">
+                                <!-- Modal content -->
+                                <div
+                                    class="relative rounded-lg shadow dark:bg-gray-700"
+                                >
+                                    <!-- Modal header -->
+                                    <div
+                                        class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
+                                    >
+                                        <h3
+                                            class="text-xl font-medium text-gray-900 dark:text-white"
+                                        >
+                                            Here's a Hint :
+                                        </h3>
+                                    </div>
+                                    <!-- Modal body -->
+                                    <div class="p-4 md:p-5 space-y-4">
+                                        <p
+                                            class="text-base leading-relaxed text-gray-500 dark:text-white"
+                                        >
+                                            {{ card.hint }}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <h3
-                            class="text-center flex flex-col items-center justify-center h-full px-2 pb-24 text-3xl font-semibold text-center"
+                            class="flex flex-col items-center justify-center h-full pb-10 text-3xl font-semibold text-center"
                         >
                             {{ card.question }}
                         </h3>
                     </div>
 
-                    <p>{{ deck.question }}</p>
                     <div
                         class="absolute flip-card backface-hidden w-full h-full dark:bg-sky-950 dark:border-sky-950 rounded-lg"
                     >
@@ -78,21 +132,21 @@ const isPrevButtonDisabled = computed(() => activeCardIndex.value === 0);
                 </div>
             </div>
         </div>
+        <div class="absolute flex justify-center w-full">
+            <button
+                class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+                @click="showPrevCard"
+                :disabled="isPrevButtonDisabled"
+                :class="{ visible: isActive, hidden: !isActive }"
+            >
+                Prev
+            </button>
+            <button
+                class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+                @click="showNextCard"
+            >
+                Next
+            </button>
+        </div>
     </AuthenticatedLayout>
-    <div class="inline-flex">
-        <button
-            class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
-            @click="showPrevCard"
-            :disabled="isPrevButtonDisabled"
-            :class="{ visible: isActive, hidden: !isActive }"
-        >
-            Prev
-        </button>
-        <button
-            class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
-            @click="showNextCard"
-        >
-            Next
-        </button>
-    </div>
 </template>
