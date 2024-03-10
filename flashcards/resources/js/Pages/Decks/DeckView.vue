9f1649a8f669326db5be3296990fbd9e9599ece5
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2024-03-10 18:50:37 -0400

    added edit title modal

--- flashcards/resources/js/Pages/Decks/DeckView.vue
+++ flashcards/resources/js/Pages/Decks/DeckView.vue
@@ -1,14 +1,19 @@
 <script setup>
 import { ref, computed } from "vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { router } from "@inertiajs/vue3";
+import { router, useForm } from "@inertiajs/vue3";
 const { deck } = defineProps(["deck"]);
+
+const form = useForm({
+    title: deck.title,
+});
 
 // Create an array to store the flipped state for each card
 const flippedStates = ref(new Array(deck.cards.length).fill(false));
 const activeCardIndex = ref(0); // Keep track of the index of the currently active card
 const isActive = ref(false);
 const showHint = ref(false);
+const showEditTitle = ref(false);
 
 const pageSize = 10;
 const currentPage = ref(1);
@@ -38,9 +43,11 @@ const showPrevCard = () => {
 const isPrevButtonDisabled = computed(() => activeCardIndex.value === 0);
 
 const hintToggle = () => {
-    console.log(showHint.value);
     showHint.value = !showHint.value;
-    console.log(showHint.value);
+};
+
+const showEditTitleModal = () => {
+    showEditTitle.value = !showEditTitle.value;
 };
 
 const shuffleCards = () => {
@@ -90,6 +97,12 @@ const onPageChange = (page) => {
 // Computed property to get total number of pages
 const totalPages = computed(() => Math.ceil(deck.cards.length / pageSize));
 
+const editTitle = async () => {
+    form.post(route("deck.editDeckTitle"), {
+        onSuccess: () => form.reset(),
+    });
+};
+
 const deleteDeck = async () => {
     try {
         await router.delete(route("deck.destroy", { deck: deck.id }));
@@ -106,14 +119,121 @@ const deleteDeck = async () => {
     <AuthenticatedLayout>
         <template #header>
             <div class="flex flex-row flex-wrap justify-between">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    {{ deck.title }}
-                </h2>
+                <div class="flex flex-col">
+                    <div
+                        class="flex flex-row justify-center items-center gap-2"
+                    >
+                        <h2
+                            class="font-semibold text-xl text-gray-800 leading-tight"
+                        >
+                            {{ deck.title }}
+                        </h2>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="black"
+                            class="w-5 h-5 cursor-pointer"
+                            @click.stop="showEditTitleModal"
+                        >
+                            <path
+                                d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z"
+                            />
+                            <path
+                                d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z"
+                            />
+                        </svg>
+                    </div>
+
+                    <!-- Main modal -->
+                    <div v-if="showEditTitle" class="absolute mt-5">
+                        <div class="relative mt-2 z-10">
+                            <!-- Modal content -->
+                            <div
+                                class="relative bg-white rounded-lg shadow dark:bg-gray-700"
+                            >
+                                <!-- Modal header -->
+                                <div
+                                    class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
+                                >
+                                    <h3
+                                        class="text-lg font-semibold text-gray-900 dark:text-white"
+                                    >
+                                        Edit Deck Title
+                                    </h3>
+                                    <button
+                                        @click.stop="showEditTitleModal"
+                                        type="button"
+                                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                    >
+                                        <svg
+                                            class="w-3 h-3"
+                                            aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 14 14"
+                                        >
+                                            <path
+                                                stroke="currentColor"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                            />
+                                        </svg>
+                                        <span class="sr-only">Close modal</span>
+                                    </button>
+                                </div>
+                                <!-- Modal body -->
+                                <form
+                                    class="p-4 md:p-5"
+                                    @submit.prevent="editTitle"
+                                >
+                                    <div class="grid gap-4 mb-4 grid-cols-2">
+                                        <div class="col-span-2">
+                                            <label
+                                                for="title"
+                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                                >{{ deck.ttile }}</label
+                                            >
+                                            <input
+                                                type="text"
+                                                v-model="form.title"
+                                            />
+                                            <div v-if="form.errors.title">
+                                                {{ form.errors.title }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button
+                                        type="submit"
+                                        :disabled="form.processing"
+                                        class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    >
+                                        Submit
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <button
-                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                    class="flex flex-row gap-2 justify-center items-center focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                     @click="deleteDeck(deck.id)"
                 >
                     Delete Deck
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        class="w-4 h-4"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
                 </button>
             </div>
         </template>
